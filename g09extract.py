b5845d75a907eccd2f637e7d466da72b4ebecc5a
Unknown <ty13946@my.bristol.ac.uk>
2019-10-23 10:42:26 +0100

    Updated g09extract

--- g09extract.py
+++ g09extract.py
@@ -5,7 +5,7 @@ import argparse
 def get_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", action='store', help='.log  file to extract E, H, G from')
+    parser.add_argument("filename", action='store', help='.log  file to extract E, H, G & charges from')
 
     return parser.parse_args()
 
@@ -18,15 +18,39 @@ def print_ehg_lumo_homo(filename):
     last_occ_eigenvalue = None
     first_virt_eigenvalue = None
 
+    mul_charges = []
+    mul_charges_section = False
+
+    nbo_charges = []
+    nbo_charges_section = False
+
     log_file_lines = [line for line in open(filename, 'r')]
 
-    for line in log_file_lines:
+    for n, line in enumerate(log_file_lines):
         if 'SCF Done' in line:
             last_scf = line.split()[4]
         if 'Sum of electronic and thermal Enthalpies' in line:
             h = line.split()[-1]
         if 'Sum of electronic and thermal Free Energies' in line:
             g = line.split()[-1]
+
+        if 'Mulliken charges and spin densities:' in line:
+            mul_charges_section = True
+
+        if 'Sum of Mulliken charges' in line and mul_charges_section:
+            mul_charges_section = False
+
+        if mul_charges_section and len(line.split()) == 4:
+            mul_charges.append(float(line.split()[2]))
+
+        if 'Natural Population' in line and 'Charge' in log_file_lines[n+2]:
+            nbo_charges_section = True
+
+        if nbo_charges_section and ' * Total *' in line:
+            nbo_charges_section = False
+
+        if nbo_charges_section and len(line.split()) == 7 and line.split()[-1][-1].isdigit():
+            nbo_charges.append(float(line.split()[2]))
 
     e = last_scf
 
@@ -39,6 +63,13 @@ def print_ehg_lumo_homo(filename):
 
     e_homo = last_occ_eigenvalue
     e_lumo = first_virt_eigenvalue
+
+    if len(mul_charges) != len(nbo_charges) and len(nbo_charges) > 0:
+        print('Printing Mulliken and NBO charges to charges.csv...\n')
+        with open('charges.csv', 'w') as charges_file:
+            print('Mulliken', 'NBO', sep=',', file=charges_file)
+            for n_charge in range(len(mul_charges)):
+                print(mul_charges[n_charge], nbo_charges[n_charge], sep=',', file=charges_file)
 
     return print('E =', e, '\tH =', h, '\tG =', g, '\tE_HOMO =', e_homo, '\tE_LUMO =', e_lumo, sep='  ')
 
